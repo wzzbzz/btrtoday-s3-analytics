ed2c823ccc308ffe1832ffc7713f9cb055125e0d
Jim Williams <wzzbzz@gmail.com>
2017-10-18 14:42:02 -0400

    latest commit

--- btrtoday_analytics.php
+++ btrtoday_analytics.php
@@ -12,6 +12,150 @@ Text Domain: dontknow
 Domain Path: /languages
 */
 
+/* btr daily dashboard */
+add_action('wp_dashboard_setup', 'add_daily_podcast_downloads_meta_box');
+ 
+function add_daily_podcast_downloads_meta_box() {
+	global $wp_meta_boxes;
+	$date = date("m/d/Y",time() - 86400);
+	wp_add_dashboard_widget('custom_help_widget', 'Top 10 Downloads for ' . $date, 'btrtoday_daily_podcast_activity');
+}
+
+function btrtoday_daily_podcast_activity() {
+	?>
+	<div id="loading" style="text-align:center"><img src="http://blog.teamtreehouse.com/wp-content/uploads/2015/05/InternetSlowdown_Day.gif" width="150"></div>
+	<div id="graph"></div>
+	
+	<script>
+		var margin = {top: 40, right: 20, bottom:350, left: 40},
+        width = 560 - margin.left - margin.right,
+        height = 800 - margin.top - margin.bottom;
+
+    
+    // set the ranges
+    var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+    
+    var y = d3.scale.linear().range([height, 0]);
+    
+    // define the axis
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+    
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+
+
+    // add the SVG element
+    var svg = d3.select("#graph").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", 
+              "translate(" + margin.left + "," + margin.top + ")");
+    
+    
+	var json_url = "http://www.btrtoday.com/json/s3/daily-podcast";
+    // load the data
+    d3.json(json_url, function(error, data) {
+		jQuery("#loading").hide();
+        var total = 0;
+        // converts string to int.
+        data.forEach(function(d) {
+            d[0] = d[0];
+            d[1] = +d[1];
+            total += d[1];
+        });
+        
+        
+        
+      // scale the range of the data
+      x.domain(data.map(function(d) { return d[0]; }));
+      y.domain([0, d3.max(data, function(d) { return d[1]; })]);
+      
+        svg.append("text")
+            .attr("x", (width / 2))             
+            .attr("y", 0 - (margin.top / 2))
+            .attr("text-anchor", "middle")  
+            .style("font-size", "16px") 
+            //.style("text-decoration", "underline")  
+            .text("today's top 10 podcasts")
+            .attr("x", (width / 2))             
+            .attr("y", 0 - (margin.top / 2))
+            .attr("text-anchor", "middle")  
+            .style("font-size", "12px") 
+            //.style("text-decoration", "underline")  
+            .text("unique IPs")
+            
+      // add axis
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis)
+        .selectAll("text")
+          .style("text-anchor", "end")
+          .attr("dx", "-.8em")
+          .attr("dy", "-.55em")
+          .attr("transform", "rotate(-90)" );
+    
+      svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+        .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 5)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("File Requests");
+    
+    
+      // Add bar chart
+      svg.selectAll("bar")
+          .data(data)
+        .enter().append("rect")
+          .attr("class", "bar")
+          .attr("x", function(d) { return x(d[0]); })
+          .attr("width", x.rangeBand())
+          .attr("y", function(d) { return y(d[1]); })
+          .attr("height", function(d) { return height - y(d[1]); })
+          .on("mouseover", function() { tooltip.style("display", null); })
+          .on("mouseout", function() { tooltip.style("display", "none"); })
+          .on("mousemove", function(d) {
+              var xPosition = d3.mouse(this)[0] - 5;
+              var yPosition = d3.mouse(this)[1] - 5;
+              tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
+              tooltip.select("text").text(d[1]);
+          });
+
+          
+    // Prep the tooltip bits, initial display is hidden
+  var tooltip = svg.append("g")
+    .attr("class", "tooltip")
+    .style("display", "none");
+      
+  tooltip.append("rect")
+    .attr("width", 60)
+    .attr("height", 20)
+    .attr("fill", "white")
+    .style("opacity", 0.5);
+
+  tooltip.append("text")
+    .attr("x", 30)
+    .attr("dy", "1.2em")
+    .style("text-anchor", "middle")
+    .attr("font-size", "12px")
+    .attr("font-weight", "bold");          
+    });
+	
+	</script>
+	<?
+?>
+
+<?php
+}
+
+
 
 // menu page
 add_action('admin_menu', 'add_btrtoday_analytics_page');
@@ -28,6 +172,7 @@ function btrtoday_analytics_enqueue_script() {
     wp_enqueue_style('btrtoday-analytics-d3-css',plugin_dir_url( __FILE__ ) .'css/css.css');
      
     wp_enqueue_script('d3js', 'http://d3js.org/d3.v3.min.js');
+    wp_enqueue_script('sortable', plugin_dir_url( __FILE__ ) .'js/sorttable.js');
     
     wp_enqueue_script(
         'js-js', 
@@ -56,10 +201,10 @@ foreach($series as $s): ?>
         <option value="<?php echo $s->slug;?>"><?php echo $s->name;?></option>
         <?php endforeach;?>
     </select>
-    <label for="from">From</label>
+    <!--<label for="from">From</label>
     <input type="text" id="from" name="from">
     <label for="to">to</label>
-    <input type="text" id="to" name="to">
+    <input type="text" id="to" name="to">-->
     <button id="get_report">Go</button>
     <button id="clear">Clear</button>
     <div id="graph"></div>
@@ -67,3 +212,111 @@ foreach($series as $s): ?>
     <?php
 }   
 
+
+// menu page
+add_action('admin_menu', 'add_btrtop10_page');
+function add_btrtop10_page(){
+    add_submenu_page ( "tools.php", "BTR Top 10", "BTR Top Artists", "manage_options", "btrtoday_top10", "render_btrtop10" );
+}
+
+
+function by_subarray_count($a,$b){
+    if(count($a["post_count"]) == count($b["post_count"]))
+        return 0;
+    return count($a["post_count"])<count($b["post_count"])?1:-1;
+}
+
+function render_btrtop10(){
+    
+   $args = array(
+    'post_type' => 'listen',
+    'post_status' => 'publish',
+    'orderby' => 'date',
+    'order' => 'DESC',
+    'numberposts'=>-1,
+    // Using the date_query to filter posts from last week
+    'date_query' => array(
+        array(
+            'after' => '1 week ago'
+        )
+    )
+    );
+   
+   $title = "Past 7 days";
+   $is_range = !empty($_GET['from']) && !empty($_GET['to']);
+   if($is_range){
+        $start = split("-",$_GET['from']);
+        $end = split("-",$_GET['to']);
+        
+        $args['date_query'] = array(
+                                array(
+                                      'after'=>array('year'=>$start[0],'month'=>$start[1],'day'=>$start[2]-1),
+                                      'before'=>array('year'=>$end[0],'month'=>$end[1],'day'=>$end[2]+1)
+                                )
+                              );
+        
+        $title = $_GET['from']." - ".$_GET['to'];
+   }
+   
+   $posts = get_posts($args);
+   $artist_post_totals = array();
+   foreach($posts as $post){
+    $artists =  get_post_artists($post->ID);
+    if(!empty($artists)){
+        foreach($artists as $artist){
+            $artist_post_totals[$artist->name]["post_count"][] = $post->ID;
+        }
+    }
+    
+    $playlist = get_field("playlist",$post->ID);
+    if(!empty($playlist)){
+        foreach($playlist as $entry){
+            if(!empty($entry['artist']) && !empty($artist_post_totals[$entry['artist']])){
+                $artist_post_totals[$entry['artist']]["track_mention"][] = $entry['title'];
+            }
+        }
+     }
+   }
+   
+   uasort($artist_post_totals, 'by_subarray_count');
+
+    ?>
+<div class="wrap">
+    <h1>BTRToday Top Artists</h1>
+    <form action="tools.php" method="get">
+        <input type="hidden" name="page" value="btrtoday_top10">
+        <label for="from">From</label>
+        <input type="text" id="from" name="from">   
+        <label for="to">to</label>
+        <input type="text" id="to" name="to">
+        
+        <button type="submit">go</button>
+    </form>
+    <hr>
+    <h2 style="text-align:center;"><?php echo $title;?></h2>
+    <style>
+        table.sortable tr:nth-child(10) td {
+            border-bottom:1px solid black;
+            margin:none;
+        }
+    </style>
+    <table class="sortable" cellspacing="5" cellpadding="5">
+        <thead>
+            <th>Artist Name</th>
+            <th>Post Mentions</th>
+            <th>Playlist Track Appearances</th>
+        </thead>
+        <?php $i=0;foreach($artist_post_totals as $name=>$posts):$i++;
+        
+        ?>
+            <tr>
+
+                <td><?php echo $name;?></td>
+                <td style="text-align:center"><?php echo count($posts['post_count']);?></td>
+                <td style="text-align:center"><?php echo count($posts['track_mention']);?></td>
+        <?php endforeach;?>    
+    </table>
+
+</div>
+    <?php
+}   
--- js/js.js
+++ js/js.js
@@ -1,29 +1,47 @@
 jQuery( function($) {
-        
+var bounce,series_name;        
     $(document).ready(function(){
         
         // do d3js stuff.
         $("#get_report").click(function(){
             var series = $("#series").val(),
                 series_name = $("#series").find("option:selected").text();
-                from=$("#from").val(),
-                to=$("#to").val();
-            if (series == "" || from == "" || to== "") {
-                alert("empty value");
-                return false;
+                //from=$("#from").val(),
+                //to=$("#to").val();
+            //if (series == "" || from == "" || to== "") {
+                //alert("empty value");
+                //return false;
+            //}
+            from="null";
+            to="null";
+            if ($("#posts").length==0 || $("#posts").val()==""){
+                url = "http://www.btrtoday.com/json/s3/series/"+series;
+                getGraph(url,series_name);
             }
-            getGraph(series,from,to,series_name);
+            else{
+                url = "http://www.btrtoday.com/json/s3/post/"+$("#posts").val()+"/";
+                post_title = $("#posts option:selected").text();
+                console.log(post_title);
+                getGraph(url, post_title);
+            }
+            
         })
         
         $("#clear").click(function(){
             $("#graph").empty();
         })
         
+        $("#series").change(function(){
+            clearTimeout(bounce);
+            bounce = setTimeout(getSeriesPosts(),500);
+        });
+        
+        
     });
     
     var from = $( "#from" )
         .datepicker({
-          defaultDate: "+1w",
+          defaultDate: "-1w",
           changeMonth: true,
           numberOfMonths: 1,
           dateFormat: "yy-mm-dd"
@@ -32,7 +50,6 @@ jQuery( function($) {
           to.datepicker( "option", "minDate", getDate( this ) );
         }),
       to = $( "#to" ).datepicker({
-        defaultDate: "+1w",
         changeMonth: true,
         numberOfMonths: 1,
         dateFormat: "yy-mm-dd"
@@ -52,12 +69,39 @@ jQuery( function($) {
  
       return date;
     }
+    
+    
+    function getSeriesPosts(){
+        series = $("#series").val();
+        url = "http://www.btrtoday.com/json/series-posts/"+series;
+        $.getJSON(url,function(data){
+            if ($("#posts").length) {
+                $("#posts").remove();
+            }
+            var posts = $("<select></select>").attr("id", "posts").attr("name","posts");
+            posts.append("<option value=''>all posts</option>");
+            for(i=0;i<data.length;i++){
+                console.log(data[i]);
+                var option = $("<option></option>").attr("value",data[i].id).text(data[i].title);
+                posts.append(option);
+            }
+            console.log(posts);
+            $("#series").after(posts);
+        });
+        
+    }
+    
+
+
   } );
 
 
-function getGraph(series,from,to,series_name) {
+
+
+
+function getGraph(json_url,title) {
     // set the dimensions of the canvas
-    var margin = {top: 40, right: 20, bottom: 70, left: 40},
+    var margin = {top: 40, right: 20, bottom:240, left: 40},
         width = 960 - margin.left - margin.right,
         height = 480 - margin.top - margin.bottom;
 
@@ -88,7 +132,7 @@ function getGraph(series,from,to,series_name) {
     
     
     // load the data
-    d3.json("http://www.btrtoday.com/json/s3/"+from+"/"+to+"/"+series, function(error, data) {
+    d3.json(json_url, function(error, data) {
         var total = 0;
         // converts string to int.
         data.forEach(function(d) {
@@ -109,7 +153,7 @@ function getGraph(series,from,to,series_name) {
             .attr("text-anchor", "middle")  
             .style("font-size", "16px") 
             //.style("text-decoration", "underline")  
-            .text(series_name + " " + from + " - " + to + ": "+ total + " total file requests");
+            .text(title + ": "+ total + " total file requests");
             
       // add axis
       svg.append("g")
@@ -141,8 +185,159 @@ function getGraph(series,from,to,series_name) {
           .attr("x", function(d) { return x(d[0]); })
           .attr("width", x.rangeBand())
           .attr("y", function(d) { return y(d[1]); })
-          .attr("height", function(d) { return height - y(d[1]); });
+          .attr("height", function(d) { return height - y(d[1]); })
+          .on("mouseover", function() { tooltip.style("display", null); })
+          .on("mouseout", function() { tooltip.style("display", "none"); })
+          .on("mousemove", function(d) {
+              var xPosition = d3.mouse(this)[0] - 5;
+              var yPosition = d3.mouse(this)[1] - 5;
+              tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
+              tooltip.select("text").text(d[1]);
+          });
 
+          
+    // Prep the tooltip bits, initial display is hidden
+  var tooltip = svg.append("g")
+    .attr("class", "tooltip")
+    .style("display", "none");
+      
+  tooltip.append("rect")
+    .attr("width", 60)
+    .attr("height", 20)
+    .attr("fill", "white")
+    .style("opacity", 0.5);
+
+  tooltip.append("text")
+    .attr("x", 30)
+    .attr("dy", "1.2em")
+    .style("text-anchor", "middle")
+    .attr("font-size", "12px")
+    .attr("font-weight", "bold");          
     });
 
+}
+
+
+function getbtrTop10Artists_posts() {
+    
+    
+// set the dimensions of the canvas
+    var margin = {top: 40, right: 20, bottom:240, left: 40},
+        width = 960 - margin.left - margin.right,
+        height = 480 - margin.top - margin.bottom;
+
+    
+    // set the ranges
+    var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+    
+    var y = d3.scale.linear().range([height, 0]);
+    
+    // define the axis
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+    
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(10);
+
+
+    // add the SVG element
+    var svg = d3.select("#graph").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", 
+              "translate(" + margin.left + "," + margin.top + ")");
+    
+    
+    // load the data
+    d3.json(json_url, function(error, data) {
+        var total = 0;
+        // converts string to int.
+        data.forEach(function(d) {
+            d[0] = d[0];
+            d[1] = +d[1];
+            total += d[1];
+        });
+        
+        
+        
+      // scale the range of the data
+      x.domain(data.map(function(d) { return d[0]; }));
+      y.domain([0, d3.max(data, function(d) { return d[1]; })]);
+      
+        svg.append("text")
+            .attr("x", (width / 2))             
+            .attr("y", 0 - (margin.top / 2))
+            .attr("text-anchor", "middle")  
+            .style("font-size", "16px") 
+            //.style("text-decoration", "underline")  
+            .text(title + ": "+ total + " total file requests");
+            
+      // add axis
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis)
+        .selectAll("text")
+          .style("text-anchor", "end")
+          .attr("dx", "-.8em")
+          .attr("dy", "-.55em")
+          .attr("transform", "rotate(-90)" );
+    
+      svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+        .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 5)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("File Requests");
+    
+    
+      // Add bar chart
+      svg.selectAll("bar")
+          .data(data)
+        .enter().append("rect")
+          .attr("class", "bar")
+          .attr("x", function(d) { return x(d[0]); })
+          .attr("width", x.rangeBand())
+          .attr("y", function(d) { return y(d[1]); })
+          .attr("height", function(d) { return height - y(d[1]); })
+          .on("mouseover", function() { tooltip.style("display", null); })
+          .on("mouseout", function() { tooltip.style("display", "none"); })
+          .on("mousemove", function(d) {
+              var xPosition = d3.mouse(this)[0] - 5;
+              var yPosition = d3.mouse(this)[1] - 5;
+              tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
+              tooltip.select("text").text(d[1]);
+          });
+
+          
+    // Prep the tooltip bits, initial display is hidden
+  var tooltip = svg.append("g")
+    .attr("class", "tooltip")
+    .style("display", "none");
+      
+  tooltip.append("rect")
+    .attr("width", 60)
+    .attr("height", 20)
+    .attr("fill", "white")
+    .style("opacity", 0.5);
+
+  tooltip.append("text")
+    .attr("x", 30)
+    .attr("dy", "1.2em")
+    .style("text-anchor", "middle")
+    .attr("font-size", "12px")
+    .attr("font-weight", "bold");          
+    });
+
+}
+
+function getbtrTop10Artists_tracks() {
+//code
 }
